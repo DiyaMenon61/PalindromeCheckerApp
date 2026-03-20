63afe5b8d84246d0baee9e69c13bb4046bd25368
DiyaMenon61 <dm6267@srmist.edu.in>
2026-03-20 18:38:45 +0530

    UC9

--- PalindromeCheckApp.java
+++ PalindromeCheckApp.java
@@ -1,64 +1,25 @@
 import java.util.*;
 
-class Node {
-    char d;
-    Node next;
-
-    Node(char d) {
-        this.d = d;
-    }
-}
-
 public class PalindromeCheckApp {
 
-    static Node rev(Node h) {
-        Node p = null;
-        while (h != null) {
-            Node n = h.next;
-            h.next = p;
-            p = h;
-            h = n;
-        }
-        return p;
+    // recursive function
+    static boolean isPal(String s, int start, int end) {
+        if (start >= end)
+            return true; // base condition
+
+        if (s.charAt(start) != s.charAt(end))
+            return false;
+
+        return isPal(s, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
 
-        Node h = null, t = null;
+        boolean result = isPal(str, 0, str.length() - 1);
 
-        // create list
-        for (char c : s.toCharArray()) {
-            Node n = new Node(c);
-            if (h == null)
-                h = t = n;
-            else {
-                t.next = n;
-                t = n;
-            }
-        }
-
-        // middle
-        Node slow = h, fast = h;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // reverse + compare
-        Node r = rev(slow);
-        boolean p = true;
-
-        while (r != null) {
-            if (h.d != r.d) {
-                p = false;
-                break;
-            }
-            h = h.next;
-            r = r.next;
-        }
-
-        System.out.println(p ? "Palindrome" : "Not Palindrome");
+        System.out.println(result ? "Palindrome" : "Not Palindrome");
     }
 }
