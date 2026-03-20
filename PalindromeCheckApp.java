20cebfb8470a967a707f2d8c5abb3328fa267307
DiyaMenon61 <dm6267@srmist.edu.in>
2026-03-20 18:43:18 +0530

    UC11

--- PalindromeCheckApp.java
+++ PalindromeCheckApp.java
@@ -1,27 +1,30 @@
 import java.util.*;
 
-public class PalindromeCheckApp {
+// service class
+class PalindromeChecker {
+    boolean checkPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
 
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+
+// main class
+public class PalindromeCheckApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter string: ");
         String str = sc.nextLine();
 
-        // normalize (remove spaces + lowercase)
-        str = str.replaceAll("\\s", "").toLowerCase();
+        PalindromeChecker p = new PalindromeChecker();
+        boolean result = p.checkPalindrome(str);
 
-        int start = 0, end = str.length() - 1;
-        boolean isPal = true;
-
-        while (start < end) {
-            if (str.charAt(start) != str.charAt(end)) {
-                isPal = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-
-        System.out.println(isPal ? "Palindrome" : "Not Palindrome");
+        System.out.println(result ? "Palindrome" : "Not Palindrome");
     }
 }
