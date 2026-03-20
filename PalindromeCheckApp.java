fad4bbac21b954fbb8eb7e700ca452e8cee7c58f
DiyaMenon61 <dm6267@srmist.edu.in>
2026-03-20 18:41:37 +0530

    UC10

--- PalindromeCheckApp.java
+++ PalindromeCheckApp.java
@@ -2,24 +2,26 @@ import java.util.*;
 
 public class PalindromeCheckApp {
 
-    // recursive function
-    static boolean isPal(String s, int start, int end) {
-        if (start >= end)
-            return true; // base condition
-
-        if (s.charAt(start) != s.charAt(end))
-            return false;
-
-        return isPal(s, start + 1, end - 1);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter string: ");
         String str = sc.nextLine();
 
-        boolean result = isPal(str, 0, str.length() - 1);
+        // normalize (remove spaces + lowercase)
+        str = str.replaceAll("\\s", "").toLowerCase();
 
-        System.out.println(result ? "Palindrome" : "Not Palindrome");
+        int start = 0, end = str.length() - 1;
+        boolean isPal = true;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                isPal = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        System.out.println(isPal ? "Palindrome" : "Not Palindrome");
     }
 }
